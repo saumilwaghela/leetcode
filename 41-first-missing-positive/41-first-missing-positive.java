f87b20579d09536921301f544737473fa6033776
saumilwaghela <36496816+saumilwaghela@users.noreply.github.com>
2022-06-11 11:17:24 +0530

    Time: 227 ms (5.02%), Space: 105.7 MB (73.14%) - LeetHub

---
+++ 41-first-missing-positive/41-first-missing-positive.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        Map<Integer,Integer> myMap = new TreeMap<Integer,Integer>();
+        int largestNumber = 1;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>0){
+                myMap.put(nums[i],1);
+                if(nums[i]>largestNumber){
+                    largestNumber = nums[i];
+                }
+            }
+        }
+        int j;
+        for(j=1; j<=largestNumber;j++){
+            if(!myMap.containsKey(j)){
+                break;
+            }
+        }
+        return j;
+    }
+}
