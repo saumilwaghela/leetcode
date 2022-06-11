82497b333c43485ca34518a97410d9181851ba1b
saumilwaghela <36496816+saumilwaghela@users.noreply.github.com>
2022-06-11 11:23:47 +0530

    Time: 428 ms (5.02%), Space: 170.6 MB (13.75%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.java
+++ 41-first-missing-positive/41-first-missing-positive.java
@@ -3,7 +3,7 @@ class Solution {
         Map<Integer,Integer> myMap = new TreeMap<Integer,Integer>();
         int largestNumber = 1;
         for(int i=0;i<nums.length;i++){
-            if(nums[i]>0 && !myMap.containsKey(nums[i])){
+            if(nums[i]>0 ){
                 myMap.put(nums[i],1);
                 if(nums[i]>largestNumber){
                     largestNumber = nums[i];
