5533f7fba41d7522ee8319274c92614b04af4ae4
saumilwaghela <36496816+saumilwaghela@users.noreply.github.com>
2022-08-18 15:29:44 +0530

    Time: 129 ms (25.45%), Space: 94.8 MB (24.11%) - LeetHub

---
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int minSetSize(int[] arr) {
+HashMap<Integer,Integer> map=new HashMap<>();
+		for(int i=0;i<arr.length;i++){
+			map.put(arr[i],map.getOrDefault(arr[i],0)+1);
+		}
+		int [] freqarr=new int[100001];
+		for(int key: map.keySet()){
+			freqarr[map.get(key)]++; 
+		}
+		int size=arr.length;int count=0;
+		for(int i=freqarr.length-1;i>=0 && size>arr.length/2;){ // traverse array in rev order to greedily choose the max frequency
+			if(freqarr[i]==0){
+				i--;
+			}else{ // keep reducing the size till freq[i]==0 -> use up all high freq elements
+				while(freqarr[i]!=0 && size>arr.length/2){
+					size-=i;
+					freqarr[i]--; 
+					count++;
+				}
+				if(size<=arr.length/2) return count;
+				i--;
+			}
+		}
+		return count;
+//         Map<Integer, Integer> countMap = new HashMap<>();
+//         PriorityQueue<Integer> countValues = new PriorityQueue<>(Comparator.reverseOrder());
+//         for (int num : arr) countMap.put(num, countMap.getOrDefault(num, 0) + 1);
+//         for (int value : countMap.values()) countValues.offer(value);
+
+//         int size = arr.length; int result = 0;
+//         while (size > arr.length / 2) {
+//             size -= countValues.poll();
+//             result++;
+//         }
+
+//         return result;
+    }
+}
