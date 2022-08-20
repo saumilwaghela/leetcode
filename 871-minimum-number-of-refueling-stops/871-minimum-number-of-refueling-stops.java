3219314180f77451dbc74fd130115defdbc867ed
saumilwaghela <36496816+saumilwaghela@users.noreply.github.com>
2022-08-20 15:38:31 +0530

    Time: 5 ms (72.16%), Space: 48.4 MB (81.24%) - LeetHub

--- 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.java
+++ 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.java
@@ -1,17 +1,33 @@
 class Solution {
     public int minRefuelStops(int target, int startFuel, int[][] stations) {
-        int N = stations.length;
-        long[] dp = new long[N + 1];
-        dp[0] = startFuel;
-        for (int i = 0; i < N; ++i){
-            for (int t = i; t >= 0; --t){
-                //System.out.println("i="+i+" t="+t+"--"+Arrays.toString(dp));
-                if (dp[t] >= stations[i][0])
-                    dp[t+1] = Math.max(dp[t+1], dp[t] + (long) stations[i][1]);
+        int n=stations.length;
+        PriorityQueue <int[]>pq = new PriorityQueue<>((a,b)->(b[1]-a[1]));
+        int refill =0;
+        int i=0;
+        int distance =startFuel;
+        
+        while(distance < target){
+            while(i<n && stations[i][0]<=distance){
+                pq.offer(stations[i]);
+                ++i;
             }
+            if(pq.isEmpty()) return -1;
+            distance = distance+pq.poll()[1];
+            ++refill;
         }
-        for (int i = 0; i <= N; ++i)
-            if (dp[i] >= target) return i;
-        return -1;
+        return refill;
+        // int N = stations.length;
+        // long[] dp = new long[N + 1];
+        // dp[0] = startFuel;
+        // for (int i = 0; i < N; ++i){
+        //     for (int t = i; t >= 0; --t){
+        //         //System.out.println("i="+i+" t="+t+"--"+Arrays.toString(dp));
+        //         if (dp[t] >= stations[i][0])
+        //             dp[t+1] = Math.max(dp[t+1], dp[t] + (long) stations[i][1]);
+        //     }
+        // }
+        // for (int i = 0; i <= N; ++i)
+        //     if (dp[i] >= target) return i;
+        // return -1;
     }
 }
