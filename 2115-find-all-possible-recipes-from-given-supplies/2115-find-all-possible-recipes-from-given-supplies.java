dd5856e55780c61f7da714c57f7901327ad2659b
saumilwaghela <36496816+saumilwaghela@users.noreply.github.com>
2022-09-17 15:51:46 +0530

    Time: 735 ms (11.48%), Space: 45.6 MB (91.21%) - LeetHub

--- 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.java
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.java
@@ -1,45 +1,20 @@
 class Solution {
     public List<String> findAllRecipes(String[] recipes, List<List<String>> ingredients, String[] supplies) {
-        List <String> availableSupplies = new ArrayList<String>(Arrays.asList(supplies));
-        HashSet <String> foundRecipe = new HashSet<String>();
-        Set<String> supple=new HashSet<>(Arrays.asList(supplies));
-        Set<String>ans=new HashSet<>();
+        Set<String> availableSupplies=new HashSet<>(Arrays.asList(supplies));
+        Set<String>foundRecipe=new HashSet<>();
 
         for(int i=0;i<ingredients.size();i++){
             outer:
             for(int j=0;j<ingredients.size();j++){
-                if(ans.contains(recipes[j]))continue;//recipe has already been made
+                if(foundRecipe.contains(recipes[j]))continue;
                 for(String ingre:ingredients.get(j)){
-                    if(!supple.contains(ingre))continue outer;//As all ingredients are not present in supplies
+                    if(!availableSupplies.contains(ingre))continue outer;
                 }
-                ans.add(recipes[j]);// add it to the answer
-                supple.add(recipes[j]); //add it to the supplies list so that another recipe can use this recipe
+                foundRecipe.add(recipes[j]);
+                availableSupplies.add(recipes[j]);
             }
 
         }
-        return new ArrayList<>(ans);
-//         int newRecipeFound;
-//         do{
-//             newRecipeFound = 0;
-//             for(int i=0;i<recipes.length;i++){
-//                 if(foundRecipe.contains(recipes[i])) continue;
-//                 List<String> requiredSuppliesForRecipe = ingredients.get(i);
-//                 boolean isAllSuppliesAvaiableForRecipe = true;
-                
-//                 for(String supply:requiredSuppliesForRecipe){
-//                     if(!availableSupplies.contains(supply)){
-//                         isAllSuppliesAvaiableForRecipe=false;
-//                     }
-//                 }
-                
-//                 if(isAllSuppliesAvaiableForRecipe){
-//                     foundRecipe.add(recipes[i]);
-//                     availableSupplies.add(recipes[i]);
-//                     newRecipeFound++;
-//                 }  
-//             }
-//         }while(newRecipeFound != 0 );
-        
-//         return new ArrayList<String>(foundRecipe);
+        return new ArrayList<String>(foundRecipe);
     }
 }
