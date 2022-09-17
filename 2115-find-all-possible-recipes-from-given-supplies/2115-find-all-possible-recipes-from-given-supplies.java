975e254d91942188d6e16c668b47ebd99e36b4e7
saumilwaghela <36496816+saumilwaghela@users.noreply.github.com>
2022-09-17 15:59:27 +0530

    Time: 996 ms (7.68%), Space: 74.3 MB (61.03%) - LeetHub

--- 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.java
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.java
@@ -1,20 +1,42 @@
 class Solution {
     public List<String> findAllRecipes(String[] recipes, List<List<String>> ingredients, String[] supplies) {
-        Set<String> availableSupplies=new HashSet<>(Arrays.asList(supplies));
-        Set<String>foundRecipe=new HashSet<>();
-
-        for(int i=0;i<ingredients.size();i++){
-            outer:
-            for(int j=0;j<ingredients.size();j++){
-                if(foundRecipe.contains(recipes[j]))continue;
-                for(String ingredient:ingredients.get(j)){
-                    if(!availableSupplies.contains(ingredient))continue outer;
+        HashSet <String> availableSupplies = new HashSet<String>(Arrays.asList(supplies));
+        HashSet <String> foundRecipe = new HashSet<String>();
+        
+        int newRecipeFound;
+        do{
+            newRecipeFound = 0;
+            searchNextRecipe:
+            for(int i=0;i<recipes.length;i++){
+                if(foundRecipe.contains(recipes[i])) continue;
+                List<String> requiredSuppliesForRecipe = ingredients.get(i);
+                
+                for(String supply:requiredSuppliesForRecipe){
+                    if(!availableSupplies.contains(supply)) continue searchNextRecipe;
                 }
-                foundRecipe.add(recipes[j]);
-                availableSupplies.add(recipes[j]);
+                
+                foundRecipe.add(recipes[i]);
+                availableSupplies.add(recipes[i]);
+                newRecipeFound++;
             }
-
-        }
+        }while(newRecipeFound != 0 );
+        
         return new ArrayList<String>(foundRecipe);
+//         Set<String> availableSupplies=new HashSet<>(Arrays.asList(supplies));
+//         Set<String>foundRecipe=new HashSet<>();
+
+//         for(int i=0;i<ingredients.size();i++){
+//             outer:
+//             for(int j=0;j<ingredients.size();j++){
+//                 if(foundRecipe.contains(recipes[j]))continue;
+//                 for(String ingredient:ingredients.get(j)){
+//                     if(!availableSupplies.contains(ingredient))continue outer;
+//                 }
+//                 foundRecipe.add(recipes[j]);
+//                 availableSupplies.add(recipes[j]);
+//             }
+
+//         }
+//         return new ArrayList<String>(foundRecipe);
     }
 }
